fa7f60fa70f4b083c2488f2b6361a89765a152bf
farhan <farhan.mithani@tntra.io>
2024-04-16 15:16:16 +0530

    initial commit

--- src/feature/Counter.js
+++ src/feature/Counter.js
@@ -9,14 +9,14 @@ export const counterSlice = createSlice({
   initialState:{value:0},
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: state => {
+    increment(state){
       state.value += 1
     },
-    decrement: state => {
+    decrement(state){
       state.value -= 1
     },
     // Use the `PayloadAction` type to declare the contents of `action.payload`
-    incrementByAmount: (state, action) => {
+    incrementByAmount(state, action){
         state.value += action.payload
       },
     }
