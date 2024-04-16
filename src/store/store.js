5196d72f9bdcaccbe5b6bb5ceb834e01851a02be
farhan <farhan.mithani@tntra.io>
2024-04-16 12:55:57 +0530

    initial commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import todosReducer from '../features/todos/todosSlice'
+import todosReducer from '../feature/todosSlice'
 // import filtersReducer from '../features/filters/filtersSlice'
 
 export const store = configureStore({
