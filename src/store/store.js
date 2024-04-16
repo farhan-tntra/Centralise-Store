33895c825d66b4211a37e8707d4ce1cc7d4983eb
farhan <farhan.mithani@tntra.io>
2024-04-16 14:00:01 +0530

    initial commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todosReducer from '../feature/todoSlice'
-import { counterSlice } from '../feature/Counter'
+import counterReducer  from '../feature/Counter'
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
-    count:counterSlice
+    count:counterReducer
   }
 })
