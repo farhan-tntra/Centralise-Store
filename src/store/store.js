622f64d83202724b23cc68c4d3214182825dd5ab
farhan <farhan.mithani@tntra.io>
2024-04-16 15:26:12 +0530

    initial commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
-import { Tuple, configureStore } from '@reduxjs/toolkit'
+import {  combineReducers, configureStore } from '@reduxjs/toolkit'
 import todosReducer from '../feature/todoSlice'
 import counterReducer  from '../feature/Counter'
 import logger from 'redux-logger'
 
-export const store = configureStore({
-  reducer: {
+
+const reducers = combineReducers({
     todos: todosReducer,
     count:counterReducer
-  },
+  },)
+
+export const store = configureStore({
+  reducer: reducers,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
